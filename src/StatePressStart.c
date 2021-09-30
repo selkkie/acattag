ae500d44b7d4cc424b46b0c8acfd77603dced8f9
selkkie <narcissialynn@gmail.com>
2021-10-01 01:52:37 +0200

    Final touches for compo! Pencils down!

--- src/StatePressStart.c
+++ src/StatePressStart.c
@@ -5,12 +5,13 @@
 #include "Scroll.h"
 #include "Palette.h"
 #include "Sound.h"
+#include "Music.h"
 IMPORT_MAP(pressstart);
-
+DECLARE_MUSIC(title);
 void START() {
     	BGP_REG = PAL_DEF(3,2,1,0);
     InitScroll(BANK(pressstart), &pressstart, 0, 0);
-
+PlayMusic(title,1);
 }
 
 
@@ -19,6 +20,7 @@ void START() {
 void UPDATE() {
 
 if(KEY_TICKED(J_START)){
+    StopMusic;
    PlayFx(CHANNEL_1, 10, 0x74, 0x87, 0x53, 0x66,0x86);
    wait_vbl_done();
     SetState(StateGame);
